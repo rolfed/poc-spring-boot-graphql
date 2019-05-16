18f5a903c23a66bcd53893cf288c38939630c3bb
Danniel Rolfe <talkto@dannielrolfe.com>
2019-05-16 09:52:31 -0700

    Integrated mysql with spring boot

--- src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/repositories/PetRepository.java
+++ src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/repositories/PetRepository.java
@@ -3,6 +3,4 @@ package uk.co.benskin.graphql_spring_boot_tutorial.repositories;
 import org.springframework.data.repository.CrudRepository;
 import uk.co.benskin.graphql_spring_boot_tutorial.entities.Pet;
 
-public interface PetRepository extends CrudRepository<Pet, Long> {
-
-}
+public interface PetRepository extends CrudRepository<Pet, Long> {}
